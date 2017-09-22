69b88768593184b01682eec4d512fb70b0799864
Bhuvnesh Chaudhary <bchaudhary@pivotal.io>
2017-09-22 13:57:23 -0700

    Bump ORCA to 2.46.2
    Signed-off-by: Bhuvnesh Chaudhary <bchaudhary@pivotal.io>

--- gpAux/releng/releng.mk
+++ gpAux/releng/releng.mk
@@ -152,7 +152,9 @@ sync_tools: opt_write_test /opt/releng/apache-ant
 	-Divyrepo.user="$(IVYREPO_USER)" -Divyrepo.passwd="$(IVYREPO_PASSWD)" resolve);
 	@echo "Resolve finished";
 
-	$(BLD_WHERE_THE_LIBRARY_THINGS_ARE)="" wget -O - https://github.com/greenplum-db/gporca/releases/download/v2.46.0/bin_orca_centos5_release.tar.gz | tar zxf - -C $(BLD_TOP)/ext/$(BLD_ARCH)
+ifeq "$(findstring aix,$(BLD_ARCH))" ""
+	LD_LIBRARY_PATH='' wget -O - https://github.com/greenplum-db/gporca/releases/download/v2.46.2/bin_orca_centos5_release.tar.gz | tar zxf - -C $(BLD_TOP)/ext/$(BLD_ARCH)
+endif
 
 clean_tools: opt_write_test
 	@cd releng/make/dependencies; \
