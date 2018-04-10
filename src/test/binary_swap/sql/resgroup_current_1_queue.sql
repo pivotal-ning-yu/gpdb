f6c53f306eb7b76c93454b5641d4ab5e7e1299ed
Ning Yu <nyu@pivotal.io>
2018-04-10 08:55:52 +0800

    resgroup: add binary swap tests.

---
+++ src/test/binary_swap/sql/resgroup_current_1_queue.sql
@@ -0,0 +1,20 @@
+show gp_resource_manager;
+
+select * from pg_resgroup
+  order by oid;
+select min(reslimittype)
+  from pg_resgroupcapability
+  group by resgroupid
+  order by resgroupid;
+select groupname from gp_toolkit.gp_resgroup_config
+  order by groupid;
+select rsgname from gp_toolkit.gp_resgroup_status
+  order by groupid;
+
+create resource group rg1 with (cpu_rate_limit=10, memory_limit=10);
+alter resource group rg1 set memory_limit 20;
+alter resource group rg1 set memory_limit 10;
+drop resource group rg1;
+create resource group rg1 with (cpu_rate_limit=10, memory_limit=10);
+
+create resource group rg2 with (cpu_rate_limit=10, memory_limit=10);
