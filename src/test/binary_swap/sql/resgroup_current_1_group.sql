c84a380fb4b506a2bef58e47e0646cbd930f1020
Ning Yu <nyu@pivotal.io>
2018-04-17 11:31:56 +0800

    resgroup: alter cpu_rate_limit in binary swap test.
    Alter cpu_rate_limit instead of memory_limit in binary swap test as the
    `other` branch on pipeline will be 5.0.0-beta.7, altering memory_limit
    is not supported yet in this version.

--- src/test/binary_swap/sql/resgroup_current_1_group.sql
+++ src/test/binary_swap/sql/resgroup_current_1_group.sql
@@ -11,12 +11,12 @@ select groupname from gp_toolkit.gp_resgroup_config
 select rsgname from gp_toolkit.gp_resgroup_status
   order by groupid;
 
-alter resource group rg1 set memory_limit 20;
-alter resource group rg1 set memory_limit 10;
+alter resource group rg1 set cpu_rate_limit 20;
+alter resource group rg1 set cpu_rate_limit 10;
 drop resource group rg1;
 create resource group rg1 with (cpu_rate_limit=10, memory_limit=10);
 
-alter resource group rg2 set memory_limit 20;
-alter resource group rg2 set memory_limit 10;
+alter resource group rg2 set cpu_rate_limit 20;
+alter resource group rg2 set cpu_rate_limit 10;
 
 -- TODO: change to the resgroup
