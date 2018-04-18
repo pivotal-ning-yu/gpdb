995241ed661b0d7e06782efb55706009766d8dd0
Ning Yu <nyu@pivotal.io>
2018-04-18 17:26:24 +0800

    resgroup: update binary swap tests.
    The tests should work both after ICW or resgroup tests.

--- src/test/binary_swap/sql/resgroup_current_1_queue.sql
+++ src/test/binary_swap/sql/resgroup_current_1_queue.sql
@@ -1,14 +1,16 @@
 show gp_resource_manager;
 
 select * from pg_resgroup
+  where rsgname not like 'rg_dump_test%'
   order by oid;
-select min(reslimittype)
+select avg(reslimittype)
   from pg_resgroupcapability
-  group by resgroupid
-  order by resgroupid;
+  where reslimittype <= 1;
 select groupname from gp_toolkit.gp_resgroup_config
+  where groupname not like 'rg_dump_test%'
   order by groupid;
 select rsgname from gp_toolkit.gp_resgroup_status
+  where rsgname not like 'rg_dump_test%'
   order by groupid;
 
 create resource group rg1 with (cpu_rate_limit=10, memory_limit=10);
