ec81db2e6266ac38afb8a51ac5f715814648fbee
Shujie Zhang <shzhang@pivotal.io>
2018-07-16 13:55:29 +0800

    add a new reshuffle expression
    (cherry picked from commit 1dbfb4ab1a11fc1122b4cb8e793d02ef606b9be4)

--- src/backend/nodes/copyfuncs.c
+++ src/backend/nodes/copyfuncs.c
@@ -4901,6 +4901,18 @@ _copyDistributedBy(const DistributedBy *from)
 	return newnode;
 }
 
+
+static ReshuffleExpr *
+_copyReshuffleExpr(ReshuffleExpr *from)
+{
+	ReshuffleExpr *newnode = makeNode(ReshuffleExpr);
+	newnode->newSegs = from->newSegs;
+	COPY_NODE_FIELD(hashKeys);
+	COPY_NODE_FIELD(hashTypes);
+
+	return newnode;
+}
+
 /* ****************************************************************
  *					pg_list.h copy functions
  * ****************************************************************
@@ -5898,6 +5910,9 @@ copyObject(const void *from)
 
 		case T_DistributedBy:
 			retval = _copyDistributedBy(from);
+			break;
+		case T_ReshuffleExpr:
+			retval = _copyReshuffleExpr(from);
 			break;
 
 		default:
