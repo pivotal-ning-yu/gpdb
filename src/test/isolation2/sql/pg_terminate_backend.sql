1541876202cb27424cbe1d9f303e8eb7b8a71c02
Ning Yu <nyu@pivotal.io>
2018-08-20 17:57:10 +0800

    Rename tables in the test case.

--- src/test/isolation2/sql/pg_terminate_backend.sql
+++ src/test/isolation2/sql/pg_terminate_backend.sql
@@ -19,18 +19,18 @@ select count(*) from foo;
 
 CREATE EXTENSION IF NOT EXISTS gp_inject_fault;
 
-DROP TABLE IF EXISTS mpp29517;
-CREATE TABLE mpp29517 (c1 int, c2 int) DISTRIBUTED BY (c1);
+DROP TABLE IF EXISTS foo;
+CREATE TABLE foo (c1 int, c2 int) DISTRIBUTED BY (c1);
 
 10: BEGIN;
 
 SELECT gp_inject_fault('create_gang_in_progress', 'reset', 1);
 SELECT gp_inject_fault('create_gang_in_progress', 'suspend', 1);
 
-10&: SELECT * FROM mpp29517 a JOIN mpp29517 b USING (c2);
+10&: SELECT * FROM foo a JOIN foo b USING (c2);
 
 SELECT pg_terminate_backend(pid) FROM pg_stat_activity
- WHERE query = 'SELECT * FROM mpp29517 a JOIN mpp29517 b USING (c2);';
+ WHERE query = 'SELECT * FROM foo a JOIN foo b USING (c2);';
 
 SELECT gp_inject_fault('create_gang_in_progress', 'resume', 1);
 
