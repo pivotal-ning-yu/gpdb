9d4453ad9d0cf06283c8ea4ecf5b1d1a90927edb
Ning Yu <nyu@pivotal.io>
2018-08-20 15:34:57 +0800

    Test termination during gang creation.
    There was a double free issue which was fixed in 772bca3f29f, add test
    case for it.
    
        commit 772bca3f29fc22a462f2d43ed4f5d8f8af474063
        Author: Pengzhou Tang <ptang@pivotal.io>
        Date:   Tue Mar 20 22:06:50 2018 -0400
    
            Fix SIGSEGV issue when freeing gangs
    
            Previously, to avoid the leak of the gang if someone terminates
            the query in the middle of gang creation, we added a global pointer
            named CurrentGangCreating so the partially created gang can also be
            destroyed at the end of the transaction. However, the memory context
            named GangContext where CurrentGangCreating was created may be reset
            before CurrentGangCreating is actually destroyed and a SIGSEGV may
            occur. So this commit makes sure that CurrentGangCreating is destroyed
            ahead of other created gangs and the reset of GangContext.
    
    Co-Authored-by: Jialun Du <jdu@pivotal.io>
    Co-Authored-by: Ning Yu <nyu@pivotal.io>

--- src/test/isolation2/sql/pg_terminate_backend.sql
+++ src/test/isolation2/sql/pg_terminate_backend.sql
@@ -12,3 +12,27 @@ FROM pg_stat_activity WHERE query like 'create temp table t as select%' ORDER BY
 
 -- query backend to ensure no PANIC on postmaster
 select count(*) from foo;
+
+--
+-- SIGSEGV issue when freeing gangs
+--
+
+CREATE EXTENSION IF NOT EXISTS gp_inject_fault;
+
+DROP TABLE IF EXISTS mpp29517;
+CREATE TABLE mpp29517 (c1 int, c2 int) DISTRIBUTED BY (c1);
+
+10: BEGIN;
+
+SELECT gp_inject_fault('create_gang_in_progress', 'reset', 1);
+SELECT gp_inject_fault('create_gang_in_progress', 'suspend', 1);
+
+10&: SELECT * FROM mpp29517 a JOIN mpp29517 b USING (c2);
+
+SELECT pg_terminate_backend(pid) FROM pg_stat_activity
+ WHERE query = 'SELECT * FROM mpp29517 a JOIN mpp29517 b USING (c2);';
+
+SELECT gp_inject_fault('create_gang_in_progress', 'resume', 1);
+
+10<:
+10q
