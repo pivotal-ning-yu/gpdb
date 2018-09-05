8d897838d1b73ccc77686fbaf4a42446b0e454e9
Ning Yu <nyu@pivotal.io>
2018-09-05 12:30:46 +0800

    Protect catalog changes on master.
    To allow gpexpand to do the job without restarting the cluster we need
    to prevent concurrent catalog changes on master.  A catalog lock is
    provided for this purpose, all insert/update/delete to catalog tables
    should hold this lock in shared mode before making the changes; gpexpand
    can hold this lock in exclusive mode to 1) wait for in-progress catalog
    updates to commit/rollback and 2) prevent concurrent catalog updates.
    
    Co-authored-by: Jialun Du <jdu@pivotal.io>
    Co-authored-by: Ning Yu <nyu@pivotal.io>

---
+++ src/backend/utils/misc/gpexpand.c
@@ -0,0 +1,99 @@
+/*-------------------------------------------------------------------------
+ *
+ * gpexpand.c
+ *	  Helper functions for gpexpand.
+ *
+ *
+ * Copyright (c) 2018-Present Pivotal Software, Inc.
+ *
+ *
+ *-------------------------------------------------------------------------
+ */
+#include "postgres.h"
+
+
+#include "catalog/gp_configuration_history.h"
+#include "catalog/pg_auth_time_constraint.h"
+#include "catalog/pg_description.h"
+#include "catalog/pg_namespace.h"
+#include "catalog/pg_partition.h"
+#include "catalog/pg_partition_encoding.h"
+#include "catalog/pg_partition_rule.h"
+#include "catalog/pg_shdescription.h"
+#include "catalog/pg_stat_last_operation.h"
+#include "catalog/pg_stat_last_shoperation.h"
+#include "catalog/pg_statistic.h"
+#include "cdb/cdbutil.h"
+#include "cdb/cdbvars.h"
+#include "storage/lock.h"
+#include "utils/rel.h"
+#include "utils/relcache.h"
+#include "utils/gpexpand.h"
+
+
+/*
+ * Catalog lock.
+ */
+static LOCKTAG gp_expand_locktag =
+{
+	/* FIXME: how to fill the locktag? */
+	.locktag_field1 = 0xdead,
+	.locktag_field2 = 0xdead,
+	.locktag_field3 = 0xdead,
+	.locktag_field4 = 0xdd,
+	.locktag_type = LOCKTAG_USERLOCK,
+	.locktag_lockmethodid = USER_LOCKMETHOD,
+};
+
+/*
+ * Prevent catalog being changed during gpexpand.
+ *
+ * This should be called before any catalog changes.
+ *
+ * Will raise an error if gpexpand already in progress or complete.
+ */
+void
+gp_expand_protect_catalog_changes(Relation relation)
+{
+	LockAcquireResult	acquired;
+
+	if (Gp_role != GP_ROLE_DISPATCH)
+		/* only lock catalog updates on qd */
+		return;
+
+	if (RelationGetNamespace(relation) != PG_CATALOG_NAMESPACE)
+		/* not catalog relations */
+		return;
+
+	switch (RelationGetRelid(relation))
+	{
+		case GpSegmentConfigRelationId:
+		case GpConfigHistoryRelationId:
+		case DescriptionRelationId:
+		case PartitionRelationId:
+		case PartitionRuleRelationId:
+		case SharedDescriptionRelationId:
+		case StatLastOpRelationId:
+		case StatLastShOpRelationId:
+		case StatisticRelationId:
+		case PartitionEncodingRelationId:
+		case AuthTimeConstraintRelationId:
+			/* these catalog tables are only meaningful on qd */
+			return;
+	}
+
+	/*
+	 * The online expand util will hold this lwlock in LW_EXCLUSIVE mode.
+	 * Acquire expand lock in dontWait mode. If the lock is not available,
+	 * report error. Because online expand must be running, after that, the
+	 * cluster size has been changed, and the catalog data has been copied
+	 * to new segments, but this transaction gangs are still running on old
+	 * segments. Any catalog changes won't be copied to new segment.
+	 */
+	/* FIXME: do not re-acquire the lock */
+	acquired = LockAcquire(&gp_expand_locktag, AccessShareLock, false, true);
+	if (acquired == LOCKACQUIRE_NOT_AVAIL)
+		ereport(ERROR,
+				(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
+				 errmsg("gpexpand in progress, catalog changes are disallowed.")));
+}
---
+++ src/include/utils/gpexpand.h
@@ -0,0 +1,19 @@
+/*-------------------------------------------------------------------------
+ *
+ * gpexpand.h
+ *	  Helper functions for gpexpand.
+ *
+ *
+ * Copyright (c) 2018-Present Pivotal Software, Inc.
+ *
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#ifndef GPEXPAND_H
+#define GPEXPAND_H
+
+void gp_expand_protect_catalog_changes(Relation relation);
+
+#endif   /* GPEXPAND_H */
+
