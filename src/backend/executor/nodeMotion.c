feadd8f08e647795cc31d832254ac5f712dd77f5
Ning Yu <nyu@pivotal.io>
2018-11-09 13:49:46 +0800

    Use max numsegments of subpaths for Append node.

--- src/backend/executor/nodeMotion.c
+++ src/backend/executor/nodeMotion.c
@@ -1478,7 +1478,7 @@ doSendTuple(Motion * motion, MotionState * node, TupleTableSlot *outerTupleSlot)
 		hval = evalHashKey(econtext, node->hashExpr,
 				motion->hashDataTypes, node->cdbhash);
 
-		Assert(hval < getgpsegmentCount() && "redistribute destination outside segment array");
+		Assert(hval < node->ps.plan->flow->numsegments && "redistribute destination outside segment array");
 		
 		/* hashSegIdx takes our uint32 and maps it to an int, and here
 		 * we assign it to an int16. See below. */
