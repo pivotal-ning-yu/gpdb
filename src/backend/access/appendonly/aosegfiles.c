3c91de8f14d62f3f07ff647356feb1c369240ea1
Jesse Zhang <sbjesse@gmail.com>
2019-03-04 15:07:15 -0800

    Print out the shared snapshot in addition to the private copy
    We suspect at least one of the following two memory regions are corrupt
    during execution:
    
    - The snapshot's (backend-private) in-progress xid array
    - The shared snapshot slot corresponding to the current process (where
    we copied "snapshot" from)
    
    Printing out the shared snapshot will help us validate this hypothesis.
    Where this will _not_ help us is if the shared snapshot's in-progress
    XID array is already corrupt before we `memcpy` it.
    
    Co-authored-by: Ashwin Agrawal <aagrawal@pivotal.io>
    Co-authored-by: Jesse Zhang <sbjesse@gmail.com>

--- src/backend/access/appendonly/aosegfiles.c
+++ src/backend/access/appendonly/aosegfiles.c
@@ -2203,6 +2203,29 @@ PrintPgaosegAndGprelationNodeEntries(FileSegInfo **allseginfo, int totalsegs, bo
 
 		appendStringInfoString(msg, "])");
 
+		if (SharedLocalSnapshotSlot == NULL)
+			appendStringInfo(msg, "\nshared snapshot == NULL");
+		else if (&SharedLocalSnapshotSlot->snapshot != InvalidSnapshot &&
+			IsMVCCSnapshot(&SharedLocalSnapshotSlot->snapshot))
+		{
+			/*
+			 * while we're at it, inspect the shared snapshot to see if the private
+			 * snapshot is corrupt
+			 */
+			appendStringInfo(msg, "\nshared snapshot (xmin %d, xmax %d, xcnt %d,"
+							 " curcid %d, haveDistributed %d\n in progress array [",
+							 SharedLocalSnapshotSlot->snapshot.xmin,
+							 SharedLocalSnapshotSlot->snapshot.xmax,
+							 SharedLocalSnapshotSlot->snapshot.xcnt,
+							 SharedLocalSnapshotSlot->snapshot.curcid,
+							 SharedLocalSnapshotSlot->snapshot.haveDistribSnapshot);
+
+			for (i = 0; i < SharedLocalSnapshotSlot->snapshot.xcnt; i++)
+				appendStringInfo(msg, "%d ", SharedLocalSnapshotSlot->snapshot.xip[i]);
+
+			appendStringInfoString(msg, "])");
+		}
+
 		if (snapshot->haveDistribSnapshot)
 		{
 			DistributedSnapshotWithLocalMapping *dslm =
